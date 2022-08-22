1ef443518f0ae55e19828ef95738bca85f1497ad
Anna Kovalchuk <annazukh@gmail.com>
2022-08-22 09:53:26 +0300

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -1,15 +1,12 @@
 const validationInput = document.querySelector("#validation-input");
 
-validationInput.addEventListener("focus", () => {
-  validationInput.value = null;
-  validationInput.classList.remove("valid", "invalid");
-});
-
 validationInput.addEventListener("blur", () => {
   const validLength = validationInput.getAttribute("data-length");
   if (validationInput.value.length === Number(validLength)) {
+    validationInput.classList.remove("invalid");
     validationInput.classList.add("valid");
   } else {
+    validationInput.classList.remove("valid");
     validationInput.classList.add("invalid");
   }
 });
